df55c00c62992438363e3ab3bf08900bbb0477ad
Rafael Nunes <rafaelns7@gmail.com>
2014-07-22 11:09:02 -0300

    Projeto

---
+++ include/menu.php
@@ -0,0 +1,16 @@
+ 
+<?PHP
+echo' 
+<div class="navbar-header">
+  <div class="navbar-collapse collapse">
+  	 <ul class="nav navbar-nav">
+  	 	 <li><a href="index.php">Home</a></li>
+  	 	 <li><a href="empresa.php">Empresa</a></li>
+  	 	 <li><a href="produto.php">Produtos</a></li>
+  	 	 <li><a href="servico.php">Serviços</a></li>
+  	 	 <li><a href="contato.php">Contato</a></li>
+  	 </ul>
+  </div>       
+</div>';
+?>
+ 
